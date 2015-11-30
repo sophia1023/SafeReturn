dcad82650db0fc5df10b3e97f91df56116a2a5a9
sophia1023 <sophia5848@gmail.com>
2015-11-30 14:24:59 +0900

    parse 이용한 경찰서 찾기 1차 올림

--- app/src/main/java/com/jh/safereturn/MainActivity.java
+++ app/src/main/java/com/jh/safereturn/MainActivity.java
@@ -1,9 +1,13 @@
 package com.jh.safereturn;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnCallMain6 = (Button) findViewById(R.id.button6);
+        btnCallMain6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("onClick", "police");
+                Intent intentSubActivity = new Intent(MainActivity.this, FindPolice.class);
+                startActivity(intentSubActivity);
+            }
+        });
     }
 
     @Override
