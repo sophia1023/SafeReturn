1a4c7cf325c5a4c8a1ec746a579f936809e63a1f
jiyoung <jiyoung4646@gmail.com>
2015-11-30 14:38:55 +0900

    test

--- app/src/main/java/com/jh/safereturn/MainActivity.java
+++ app/src/main/java/com/jh/safereturn/MainActivity.java
@@ -1,17 +1,41 @@
 package com.jh.safereturn;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private String[] navItems = {"RED", "BLUE", "GREEN"};
+    private DrawerLayout dlDrawer;
+    private ListView navList;
+    private Fragment fragment;
+
+    //dlDrawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        }
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
