204715d26a87a9ede59d923f4d26d5ae16f56f7c
sophia1023 <sophia5848@gmail.com>
2015-12-07 19:32:19 +0900

    locationSMS 수정

--- app/src/main/java/com/jh/safereturn/LocationSMS.java
+++ app/src/main/java/com/jh/safereturn/LocationSMS.java
@@ -8,22 +8,16 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -40,18 +34,16 @@ import java.util.StringTokenizer;
 /**
  * Created by HUNNY on 2015-12-03.
  */
-public class LocationSMS extends FragmentActivity implements LocationListener {
+public class LocationSMS extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private GpsInfo gps;
     private PolylineOptions polylineOptions;
-    LocationManager locationManager;
-    String provider;
-    LatLng latlng;
     double latitude, longitude;
     LatLng receiveLoc;
     String receivelat, receivelon;
-    boolean locationTag = true;
     ArrayList<LatLng> arrayPoints;
+    String sender;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -59,42 +51,14 @@ public class LocationSMS extends FragmentActivity implements LocationListener {
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
 
-        int googlePlayServiceResult = GooglePlayServicesUtil.isGooglePlayServicesAvailable(LocationSMS.this);
-        if( googlePlayServiceResult !=   ConnectionResult.SUCCESS){ //구글 플레이 서비스를 활용하지 못할경우 <계정이 연결이 안되어 있는 경우
-            //실패
-            GooglePlayServicesUtil.getErrorDialog(googlePlayServiceResult, this, 0, new DialogInterface.OnCancelListener()
-            {
-                @Override
-                public void onCancel(DialogInterface dialog)
-                {
-                    finish();
-                }
-            }).show();
-        }else { //구글 플레이가 활성화 된 경우
-            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            Criteria criteria = new Criteria();
-            provider = locationManager.getBestProvider(criteria, true);
-
-            if (provider == null) {  //위치정보 설정이 안되어 있으면 설정하는 엑티비티로 이동합니다
-                new AlertDialog.Builder(LocationSMS.this)
-                        .setTitle("위치서비스 동의")
-                        .setNeutralButton("이동", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                startActivityForResult(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS), 0);
-                            }
-                        }).setOnCancelListener(new DialogInterface.OnCancelListener() {
-                    @Override
-                    public void onCancel(DialogInterface dialog) {
-                        finish();
-                    }
-                })
-                        .show();
-            } else {   //위치 정보 설정이 되어 있으면 현재위치를 받아옵니다
-                locationManager.requestLocationUpdates(provider,1,1, LocationSMS.this); //기본 위치 값 설정
-                setUpMapIfNeeded(); //Map ReDraw
-            }
-            setMyLocation(); //내위치 정하는 함수
+        gps = new GpsInfo(this);
+        // GPS 사용유무 가져오기
+        if (gps.isGetLocation()) {
+            latitude = gps.getLatitude();
+            longitude = gps.getLongitude();
+        } else {
+            // GPS 를 사용할수 없으므로
+            gps.showSettingsAlert();
         }
 
         // BitmapDescriptorFactory 생성하기 위한 소스
@@ -104,19 +68,6 @@ public class LocationSMS extends FragmentActivity implements LocationListener {
         ((Button)findViewById(R.id.sendLocation)).setOnClickListener(mClickListener);
         ((Button)findViewById(R.id.receiveLocation)).setOnClickListener(mClickListener);
     }
-
-    public void setMyLocation(){
-        mMap.setOnMyLocationChangeListener(myLocationChangeListener);
-    }
-
-   public GoogleMap.OnMyLocationChangeListener myLocationChangeListener = new GoogleMap.OnMyLocationChangeListener() {
-        @Override
-        public void onMyLocationChange(Location location) {
-            latlng = new LatLng(location.getLatitude(), location.getLongitude());
-            latitude = location.getLatitude();
-            longitude = location.getLongitude();
-        }
-    };
 
     public void init() {
         GooglePlayServicesUtil.isGooglePlayServicesAvailable(LocationSMS.this);
@@ -135,11 +86,6 @@ public class LocationSMS extends FragmentActivity implements LocationListener {
                 "android.provider.Telephony.SMS_RECEIVED"));
     }
 
-    /* public void onPause() {
-         super.onPause();
-         unregisterReceiver(mReceiverBR);
-     }
- */
     BroadcastReceiver mReceiverBR = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -152,6 +98,7 @@ public class LocationSMS extends FragmentActivity implements LocationListener {
                 for(int i = 0;i<pdus.length;i++) {
                     SmsMessage msg = SmsMessage.createFromPdu((byte [])pdus[i]);
                     result = msg.getMessageBody() + "\n";
+                    sender = msg.getOriginatingAddress();
                 }
                 stz1 = new StringTokenizer(result,"\n");
                 receivelat = stz1.nextToken();
@@ -167,7 +114,7 @@ public class LocationSMS extends FragmentActivity implements LocationListener {
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(receiveLoc, 15));
         Marker marker = mMap.addMarker(new MarkerOptions()
                 .position(receiveLoc)
-                .title("친구의 위치")
+                .title(sender)
                 .snippet("위치" + (arrayPoints.size()+1)));
 
         arrayPoints.add(receiveLoc);
@@ -246,7 +193,7 @@ public class LocationSMS extends FragmentActivity implements LocationListener {
         mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 
-    @Override
+    /*@Override
     public void onLocationChanged(Location location) {
         if(locationTag){//한번만 위치를 가져오기 위해서 tag를 주었습니다
             Log.d("myLog", "onLocationChanged: !!" + "onLocationChanged!!");
@@ -276,5 +223,5 @@ public class LocationSMS extends FragmentActivity implements LocationListener {
     public void onStatusChanged(String provider, int status, Bundle extras) {
         // TODO Auto-generated method stub
 
-    }
+    }*/
 }
